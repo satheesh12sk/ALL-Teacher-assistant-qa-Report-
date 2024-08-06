64df42ef1c6ee60945bbe54d5381c5019785de34
RameshAravindhQA <ramesharavindhkarthikeyan.qa@gmail.com>
2024-08-06 18:16:31 +0530

    Update ProjectSpecifications.java
    Two Project Specifications have came that's why I haved in both class

--- mainPackageTellPortal/ProjectSpecifications.java
+++ mainPackageTellPortal/ProjectSpecifications.java
@@ -29,8 +29,6 @@ public class ProjectSpecifications {
 
 		driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		
 		jsExecutor = (JavascriptExecutor) driver;
