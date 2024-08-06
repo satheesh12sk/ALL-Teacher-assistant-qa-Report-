32393d10cbcd4ea032fdd6270a85f6d4b589ab3f
RameshAravindhQA <ramesharavindhkarthikeyan.qa@gmail.com>
2024-08-06 18:18:35 +0530

    Update TC_002_Login_TellPortal.java
    Changes have made

--- testCasePackageTellPortal/TC_002_Login_TellPortal.java
+++ testCasePackageTellPortal/TC_002_Login_TellPortal.java
@@ -24,15 +24,12 @@ public class TC_002_Login_TellPortal extends ProjectSpecifications {
 
 	@Test(dataProvider = "loginData")
 	public void validatelogin(String usernamevalues, String passwordvalues) throws InterruptedException {
-
+              // Entering the username
 		WebElement email = driver.findElement(By.xpath("//input[@name='emailOrPhoneNumber']"));
-
-		// Entering the username
 		email.sendKeys(usernamevalues);
-
+		
+                // Entering the password
 		WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
-
-		// Entering the password
 		password.sendKeys(passwordvalues);
 
 		// Click login Button
